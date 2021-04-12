842684f94d3c00988680cd07efe828fc5b37294b
David Stone <daystone4@gmail.com>
2021-04-11 21:20:08 -0400

    First Commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,42 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
+    counter: 0,
+    colorCode: '',
+
+  },
+  getters: {
+    counterSquared(state) { // we want to grab something from the state and modify it, getters return something
+      return state.counter * state.counter
+    }
   },
   mutations: {
+    increaseCounter(state, randomNumber) {
+      state.counter+= randomNumber
+    },
+    decreaseCounter(state, randomNumber) {
+      state.counter-= randomNumber
+    },
+    setColorCode(state, newValue) {
+      state.colorCode = newValue
+    }
   },
   actions: {
+    async increaseCounter({ commit }) { // in order to commit a mutation from an action, we need to pass an object into the action with the commit method, this is an example
+      const res = await axios.get('https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new')
+      commit('increaseCounter', res.data) // commit the name of the mutation and the payload to be passed to the mutation
+
+    },
+    async decreaseCounter({ commit }) { 
+      const res = await axios.get('https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new')
+      commit('decreaseCounter', res.data) 
+
+    },
+    setColorCode({ commit }, newValue){
+      commit('setColorCode', newValue)
+    }
   },
   modules: {
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <Counter />
+  <CounterSquared />
+  <Buttons />
+  <ColorCode />
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Counter from '@/components/Counter'
+import CounterSquared from '@/components/CounterSquared'
+import Buttons from '@/components/Buttons'
+import ColorCode from '@/components/ColorCode'
+
 
 export default {
   name: 'Home',
+  
   components: {
-    HelloWorld
+    Counter,
+    CounterSquared,
+    Buttons,
+    ColorCode,
   }
 }
 </script>
+
+<style >
+div {
+  margin-bottom: 10px;
+}
+
+.counter  {
+  font-size: 80px;
+}
+
+.buttons button {
+  font-size: 40px;
+  width: 100px;
+  margin: 0 10px;
+}
+</style>
